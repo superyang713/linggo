f2580de9ec044a68c0b88508cf9a040c4cf339ba
Yang Dai <yang.dai2020@gmail.com>
2020-01-14 05:06:46 -0500

    update

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS, cross_origin
 
 from app.config import Config, ConfigDb
 
@@ -11,6 +12,8 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 app.config.from_object(ConfigDb)
+
+CORS(app, support_credentials=True)
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
--- app/resources/user.py
+++ app/resources/user.py
@@ -3,6 +3,14 @@ from webargs import fields
 from webargs.flaskparser import use_args, use_kwargs
 
 from app.models import User as U
+from app import ma
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("name", "password")
+    name = fields.Str(required=True, location="json")
+    password = fields.Str(required=True, location="json")
 
 
 class User(Resource):
@@ -22,3 +30,10 @@ class User(Resource):
         db.session.commit()
 
         return {"message": "successful"}, 201
+
+    def get(self):
+        user_schema = UserSchema(many=True)
+        all_entries = U.query.all()
+
+        result = user_schema.dump(all_entries)
+        return {"users": result}
